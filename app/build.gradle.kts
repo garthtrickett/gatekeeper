1f906e8c675886d15f3d5be8669c3f2af4333c78
Garth Trickett <garthtrickett@gmail.com>
2026-04-14 21:30:34 +1000

    Switching to the idiomatic Kotlin DSL for the Compose plugin to ensure Android Gradle Plugin resolves it correctly.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    kotlin("android")
+    kotlin("plugin.compose")
     id("app.cash.sqldelight")
 }
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     // These versions match the Android SDK 34/35 defined in our Nix Flake
     id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
+    kotlin("android") version "2.1.0" apply false
+    kotlin("plugin.compose") version "2.1.0" apply false
     id("app.cash.sqldelight") version "2.0.2" apply false
 }
