9d786b255b17c6144e91a482330bbeb95ef7be0e
Garth Trickett <garthtrickett@gmail.com>
2026-04-23 17:30:02 +1000

    Fix syntax and missing method errors in GatekeeperReducerTest caused by malformed patch application. Added missing brace, removed duplicate test function, and updated legacy action reference.

--- app/src/test/java/com/aegisgatekeeper/app/domain/GatekeeperReducerTest.kt
+++ app/src/test/java/com/aegisgatekeeper/app/domain/GatekeeperReducerTest.kt
@@ -705,6 +705,8 @@ class GatekeeperReducerTest {
 
         val state2 = reduce(state1, GatekeeperAction.ClearIntentionalSlot(slotIndex = 2))
         assertThat(state2.intentionalSlots).isEmpty()
+    }
+
     @Test
     fun testOpenCleanAudioPlayer_SetsActiveAudioUrl() {
         val newState = reduce(initialState, GatekeeperAction.OpenCleanAudioPlayer("https://soundcloud.com/test"))
@@ -713,12 +715,6 @@ class GatekeeperReducerTest {
     }
 
     @Test
-    fun testStopCleanAudioPlayer_ClearsActiveAudioUrl() {
-        val activeState = initialState.copy(activeAudioUrl = "https://soundcloud.com/test", isAudioPlayerModalVisible = true)
-        val newState = reduce(activeState, GatekeeperAction.StopCleanAudioPlayer)
-        assertThat(newState.activeAudioUrl).isNull()
-        assertThat(newState.isAudioPlayerModalVisible).isFalse()
-    }
     fun testStopCleanAudioPlayer_ClearsActiveAudioUrl() {
         val activeState = initialState.copy(activeAudioUrl = "https://soundcloud.com/test", isAudioPlayerModalVisible = true)
         val newState = reduce(activeState, GatekeeperAction.StopCleanAudioPlayer)
