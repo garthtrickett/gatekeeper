deeaeb0e9be839c70c0f00e38acccb3cb866a76b
Garth Trickett <garthtrickett@gmail.com>
2026-04-24 13:44:09 +1000

    Add missing import for bodyAsText in PodcastIndexClient.

--- app/src/main/java/com/aegisgatekeeper/app/api/PodcastIndexClient.kt
+++ app/src/main/java/com/aegisgatekeeper/app/api/PodcastIndexClient.kt
@@ -10,6 +10,7 @@ import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.client.request.parameter
+import io.ktor.client.statement.bodyAsText
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
