26f2e5369cf6b3b50a12867bf0dbf6c241ddb9e6
Garth Trickett <garthtrickett@gmail.com>
2026-04-24 17:46:54 +1000

    Fixes a compilation error in `CleanYouTubeScreenTest.kt` by removing duplicated import statements for `assertExists` and `assertIsDisplayed`. The previous attempt to add these imports resulted in duplicates. This change cleans up the imports to resolve the `Unresolved reference` error.

--- app/src/androidTest/java/com/aegisgatekeeper/app/views/CleanYouTubeScreenTest.kt
+++ app/src/androidTest/java/com/aegisgatekeeper/app/views/CleanYouTubeScreenTest.kt
@@ -2,8 +2,6 @@ package com.aegisgatekeeper.app.views
 
 import androidx.compose.ui.test.assertExists
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.assertExists
-import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
