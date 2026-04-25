d9abfe1f2909e169c2dd1397f3b16ef9deb18236
Garth Trickett <garthtrickett@gmail.com>
2026-04-25 19:30:03 +1000

    Fix missing LaunchedEffect import in SettingsScreen and DesktopAccountScreen

--- app/src/main/java/com/aegisgatekeeper/app/views/SettingsScreen.kt
+++ app/src/main/java/com/aegisgatekeeper/app/views/SettingsScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import com.aegisgatekeeper.app.domain.IndustrialButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
