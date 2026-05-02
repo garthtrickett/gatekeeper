ac70ff2064cf903411f5672166ea0c98886ad3f2
Garth Trickett <garthtrickett@gmail.com>
2026-05-02 11:34:11 +1000

    Fix duplicated code in SharedApplicationComponent.kt causing compilation error.

--- app/src/commonMain/kotlin/com/aegisgatekeeper/app/di/SharedApplicationComponent.kt
+++ app/src/commonMain/kotlin/com/aegisgatekeeper/app/di/SharedApplicationComponent.kt
@@ -14,20 +14,4 @@ interface SharedApplicationComponent {
     val syncClient: SyncClient
     val beeperClient: BeeperClient
 }
-package com.aegisgatekeeper.app.di
-
-import com.aegisgatekeeper.app.auth.TokenProvider
-import com.aegisgatekeeper.app.integrations.BeeperClient
-import com.aegisgatekeeper.app.sync.SyncClient
-import me.tatarka.inject.annotations.Scope
-
-@Scope
-@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER)
-annotation class Singleton
-
-interface SharedApplicationComponent {
-    val tokenProvider: TokenProvider
-    val syncClient: SyncClient
-    val beeperClient: BeeperClient
-}
 
