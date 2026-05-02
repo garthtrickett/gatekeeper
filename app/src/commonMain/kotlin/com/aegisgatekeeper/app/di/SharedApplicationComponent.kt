8e0dde4029ab69c3e9e209787921c800403a9bf5
Garth Trickett <garthtrickett@gmail.com>
2026-05-02 11:32:23 +1000

    Fix compilation error by correctly placing the BeeperClient import at the top of SharedApplicationComponent.

--- app/src/commonMain/kotlin/com/aegisgatekeeper/app/di/SharedApplicationComponent.kt
+++ app/src/commonMain/kotlin/com/aegisgatekeeper/app/di/SharedApplicationComponent.kt
@@ -1,14 +1,13 @@
 package com.aegisgatekeeper.app.di
 
 import com.aegisgatekeeper.app.auth.TokenProvider
+import com.aegisgatekeeper.app.integrations.BeeperClient
 import com.aegisgatekeeper.app.sync.SyncClient
 import me.tatarka.inject.annotations.Scope
 
 @Scope
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER)
 annotation class Singleton
-
-import com.aegisgatekeeper.app.integrations.BeeperClient
 
 interface SharedApplicationComponent {
     val tokenProvider: TokenProvider
