69a7fd85d26d067105f4945b95160cd6d27e2a76
Garth Trickett <garthtrickett@gmail.com>
2026-05-09 12:28:57 +1000

    Fix syntax errors and code duplication in WebAppInterface.kt.

--- app/src/main/java/com/aegisgatekeeper/app/views/WebAppInterface.kt
+++ app/src/main/java/com/aegisgatekeeper/app/views/WebAppInterface.kt
@@ -5,7 +5,7 @@ import android.os.Looper
 import android.webkit.JavascriptInterface
 
 /** A lightweight bridge to receive state change events from the YouTube IFrame Player API. */
-class WebAppInterface(
+class WebAppInterface(\n    private val onVideoEnded: () -> Unit,\n    private val onStateChangeCallback: (Int) -> Unit = {},\n    private val onTimeUpdateCallback: (Float) -> Unit = {},\n) {\n    @JavascriptInterface\n    fun logMessage(message: String) {\n        android.util.Log.d("Gatekeeper.CleanPlayerJS", message)\n    }\n\n    @JavascriptInterface\n    fun onTimeUpdate(time: String) {\n        val parsedTime = time.toFloatOrNull() ?: 0f\n        Handler(Looper.getMainLooper()).post { onTimeUpdateCallback(parsedTime) }\n    }\n\n    /**\n     * This method is called from the JavaScript inside the WebView.\n     * @param playerState The state code from the YouTube player. 0 means the video has ended.\n     */\n    @JavascriptInterface\n    fun onStateChange(playerState: Int) {\n        // Must run on main thread to update Compose state\n        Handler(Looper.getMainLooper()).post {\n            onStateChangeCallback(playerState)\n            if (playerState == 0) {\n                onVideoEnded()\n            }\n        }\n    }\n\n    /**\n     * Logs player errors from the WebView to Android's Logcat for better diagnostics. See:\n     * https://developers.google.com/youtube/iframe_api_reference#onError\n     */\n    @JavascriptInterface\n    fun logError(errorCode: Int) {\n        android.util.Log.e("Gatekeeper.YouTubePlayer", "Received internal player error: $errorCode")\n    }\n}\n\n
     private val onVideoEnded: () -> Unit,
     private val onStateChangeCallback: (Int) -> Unit = {},
     private val onTimeUpdateCallback: (Float) -> Unit = {},
